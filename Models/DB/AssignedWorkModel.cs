214a76d8c6dbbed2c7d7fe4aaf5615c60cd8eddf
KraSS <groverkras@gmail.com>
2023-09-19 16:14:23 +0300

    Create: AssignedWorkModel, TaskModel

---
+++ Models/DB/AssignedWorkModel.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace api.Models.DB
+{
+    [Table("Assigned_work")]
+    public class AssignedWorkModel : BaseModel
+    {
+        [Column("mentor_id")]
+        [Required]
+        public Ulid MentorId { get; set; }
+
+        [Column("student_id")]
+        [Required]
+        public Ulid StudentId { get; set; }
+
+        [Column("work_id")]
+        [Required]
+        public Ulid WorkId { get; set; }
+
+        [Column("deadline_at")]
+        public DateTime DeadLineAt { get; set; }
+
+        protected override void GenerateSlug()
+        {
+            // TODO: Implement WorkModel slug
+        }
+    }
+}
---
+++ Models/DB/TaskModel.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using api.Models.Enums;
+
+namespace api.Models.DB
+{
+    public class TaskModel : BaseModel
+    {
+        [Column("work_id")]
+        [Required]
+        public Ulid WorkId { get; set; }
+
+        [Column("name")]
+        [MaxLength(255)]
+        public string? Name { get; set; }
+
+        [Column("content")]
+        public string? Content { get; set; }
+
+        [Column("highest_score")]
+        public int HighestScore { get; set; }
+
+        [Column("type")]
+        public TaskType Type { get; set; }
+
+        protected override void GenerateSlug()
+        {
+            // TODO: Implement WorkModel slug
+        }
+    }
+}
