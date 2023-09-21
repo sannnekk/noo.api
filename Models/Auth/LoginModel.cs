278b35ba0e5b05faea09dd61acdabad819e5f8a8
KraSS <groverkras@gmail.com>
2023-09-21 21:02:09 +0300

    Create IAuthService

---
+++ Models/Auth/LoginModel.cs
@@ -0,0 +1,9 @@
+﻿namespace api.Models.Auth
+{
+    public class LoginModel
+    {
+        public string UserName { get; set; } = string.Empty;
+
+        public string Password { get; set; } = string.Empty;
+    }
+}
---
+++ Services/Interfaces/IAuthService.cs
@@ -0,0 +1,20 @@
+﻿using api.Models.Auth;
+using api.Models.DB;
+
+namespace api.Services.Interfaces
+{
+    public interface IAuthService
+    {
+        /// <summary>
+        /// Check whether such a user exists
+        /// </summary>        
+        UserModel Authenticate(LoginModel model);
+
+        /// <summary>
+        /// Save in claims all necessary information
+        /// and generate JWT token
+        /// </summary>      
+        /// <returns>JWT token as a string</returns>
+        string GenerateJWTToken(UserModel model);
+    }
+}
